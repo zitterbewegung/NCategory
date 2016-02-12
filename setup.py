930eb90088ad9ad3f8ed46d7db552209b4bbe190
Joshua Herman <zitterbewegung@gmail.com>
2016-02-12 11:05:36 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='mrfantastic',
+    version='0.1dev',
+    description='This is https://github.com/mozilla/mrfantastic',
+    author='A Cons Apart LLC',
+    author_email='',
+    url='https://github.com/mozilla/mrfantastic'
+)
