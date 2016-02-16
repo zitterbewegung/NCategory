0982555596c23d2e1c232f0c4d56f735a6cdd81f
Joshua Herman <zitterbewegung@gmail.com>
2016-02-15 22:57:39 -0600

    Made raven/ sentry client work. Improved model to track print jobs.

--- mrfantastic/simplex/models.py
+++ mrfantastic/simplex/models.py
@@ -3,16 +3,53 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.contrib.auth.models import User
+from raven.contrib.django.raven_compat.models import client
 
-# Create your models here.
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 
+    # Create your models here.
+    # This is an auto-generated Django model module.
+    # You'll have to do the following manually to clean this up:
+    #   * Rearrange models' order
+    #   * Make sure each model has one field with primary_key=True
+    #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+    #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+    # Feel free to rename the models, but don't rename db_table values or field names.
+
+class Comment(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    body = models.TextField()
+    # author = models.ForeignKey('Account')
+    print_field = models.ForeignKey('Print', models.DO_NOTHING, db_column='print')  # Field renamed because it was a Python reserved word.
+    created_at = models.DateTimeField()
+
+class Address(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    last_name = models.TextField()
+    first_name = models.TextField()
+    address = models.TextField()
+    address2 = models.TextField()
+    city = models.TextField()
+    zipcode = models.CharField(max_length=10)
+    mail_state = models.TextField()
+    country = models.TextField()
+    
+class Tag(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    category = models.TextField()
+    confidence = models.FloatField()
+
+    
+class Print(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    title = models.TextField()
+    description = models.TextField()
+    file_path = models.TextField()
+    image_file = models.TextField()
+    thumbnail_file = models.TextField()
+    print_file = models.TextField()
+    price = models.DecimalField(max_digits=19, decimal_places=4)
+    tags = models.ManyToManyField(Tag) 
+    created_at = models.DateTimeField()
 
 class Account(models.Model):
     id = models.BigIntegerField(primary_key=True)
@@ -25,68 +62,13 @@ class Account(models.Model):
     city = models.TextField()
     date_created = models.DateTimeField()
     modified_date = models.DateTimeField()
+    address = models.ManyToManyField(Address)
+    prints = models.ManyToManyField(Print)
+    comments = models.ManyToManyField(Comment)
 
-    class Meta:
-        managed = False
- 
-
-class AccountMailing(models.Model):
-    email = models.TextField()
-    mailing_id = models.IntegerField(primary_key=True)
+class Job(models.Model):
+    mailing_address = models.ForeignKey(Account, on_delete=models.CASCADE)    
+    to_print = models.ForeignKey(Print, on_delete=models.CASCADE)
     date_created = models.DateTimeField()
-
-    class Meta:
-        db_table = 'accounts_mailing'
-
-
-class AccountPrint(models.Model):
-    email = models.TextField()
-    print_field = models.BigIntegerField(db_column='print', blank=True, null=True)  # Field renamed because it was a Python reserved word.
-    date_created = models.DateTimeField()
-
-    class Meta:
-        db_table = 'accounts_prints'
-
-
-class Comment(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    body = models.TextField()
-    author = models.ForeignKey('Account')
-    print_field = models.ForeignKey('Print', models.DO_NOTHING, db_column='print')  # Field renamed because it was a Python reserved word.
-    created_at = models.DateTimeField()
-
-    class Meta:
-        db_table = 'comments'
-
-
-class Mailing(models.Model):
-    recipient = models.ForeignKey('Account' , on_delete=models.CASCADE)
-    id = models.BigIntegerField(primary_key=True)
-    last_name = models.TextField()
-    first_name = models.TextField()
-    address = models.TextField()
-    address2 = models.TextField()
-    city = models.TextField()
-    zipcode = models.CharField(max_length=10)
-    mail_state = models.TextField()
-    country = models.TextField()
-
-    class Meta:
-        db_table = 'mailing'
-
-
-class Print(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    author = models.OneToOneField('Account', on_delete=models.CASCADE)
-    title = models.TextField()
-    description = models.TextField()
-    file_directory = models.TextField()
-    file = models.TextField()
-    image_file = models.TextField()
-    thumbnail_file = models.TextField()
-    print_file = models.TextField()
-    price = models.DecimalField(max_digits=19, decimal_places=4)
-    tags = models.TextField()  # This field type is a guess.
-
-    class Meta:
-        db_table = 'prints'
+    
+    
