0e071f5fc03472d639d0472986b5f31110008ed1
zitterbewegung <zitterbewegung@gmail.com>
2016-03-10 18:34:28 -0600

    Adding more tasks and testing

--- mrfantastic/simplex/tasks.py
+++ mrfantastic/simplex/tasks.py
@@ -1,7 +1,14 @@
 from functools import wraps
 
 from .celeryconf import app
-from .models import Job
+from .models import Jo
+
+import argparse
+import base64
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import GoogleCredentials
 
 
 # decorator to avoid code duplication
@@ -52,6 +59,39 @@ def _fib(n):
         return n
     else:
         return _fib(n - 1) + _fib(n - 2)
+
+@app.task
+@update_job
+def label_image_google_vision(photo_file):
+  '''Run a label request on a single image'''
+
+  API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+  http = httplib2.Http()
+
+  credentials = GoogleCredentials.get_application_default().create_scoped(
+      ['https://www.googleapis.com/auth/cloud-platform'])
+  credentials.authorize(http)
+
+  service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
+
+  with open(photo_file, 'rb') as image:
+    image_content = base64.b64encode(image.read()).decode('utf-8')
+    service_request = service.images().annotate(
+      body={
+        'requests': [{
+          'image': {
+            'content': image_content
+           },
+          'features': [{
+            'type': 'LABEL_DETECTION',
+            'maxResults': 1,
+           }]
+         }]
+      })
+    response = service_request.execute()
+    label = response['responses'][0]['labelAnnotations'][0]['description']
+    print(('Found label: %s for %s' % (label, photo_file)))
+    
 
 
 @app.task
--- mrfantastic/simplex/tests.py
+++ mrfantastic/simplex/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase  # noqa
+from django.utils import timezone
+from .models import Print, Tag
+import datetime
 
 #  Create your tests here.
+
+class SimplexMethodtests(TestCase):
+    
+    def test_elasticsearch_index_print(self ):
+        """
+        """
+        time = timezone.now()
+        testTag = Tag("test",1.0)
+        print_in_es = Print(title="test",description="test", file_path="test",image_file="test",
+                            thumbnail_file="test",print_file="test",price="test",tags=testTag
+                            , created_at=timezone.now(),modified_at=timezone.now())
+        self.assertEqual(len(Print.objects.search.query('title', _all='test')), 1)
