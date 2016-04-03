d568f51dd08cb20e51ae169318cfc933615c45a6
zitterbewegung <zitterbewegung@gmail.com>
2016-04-03 17:00:04 -0500

    Fixed tests
    Forgot to capitalize test assertEqual

--- mrfantastic/simplex/tests.py
+++ mrfantastic/simplex/tests.py
@@ -16,5 +16,5 @@ class SimplexMethodtests(TestCase):
         print_in_es = Print(title="test", description="test", file_path="test", image_file="test",
                             thumbnail_file="test", print_file="test", price="test", tags=testTag,
                             created_at=timezone.now(), modified_at=timezone.now())
-        self.assertequal(print_in_es.title, "test")
+        self.assertEqual(print_in_es.title, "test")
         # self.assertEqual(len(Print.objects.search.query('title', _all='test')), 1)
