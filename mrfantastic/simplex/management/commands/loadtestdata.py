d6b54dece54f0f37959f7b8523211430bca33a3c
zitterbewegung <zitterbewegung@gmail.com>
2016-08-21 22:29:53 -0500

    Added more documentation to loadtestdata

--- mrfantastic/simplex/management/commands/loadtestdata.py
+++ mrfantastic/simplex/management/commands/loadtestdata.py
@@ -2,14 +2,18 @@ from mrfantastic.simplex.models import Print, Tag, Account
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from mrfantastic.simplex.utility import pcd_to_vfh_histogram, convert_obj_pcd
+from django.conf import settings
+from django.contrib.auth.models import User
+
 
 class Command(BaseCommand):
 
-    help = "Whatever you want to print here"
-
+    help = "Creates an admin user and initializes database with data"
+    
     def handle(self, *args, **options):
         data_dir = '/app/data/GroundTruthDatabase/'
         serving_path = "/static/GroundTruthDatabase/"
+
         with open(data_dir + 'all_1833.txt', 'r') as infile:
             for line in infile:
                 thumbnail = serving_path + line.strip().replace("\\", "/") + ".jpg"
@@ -17,7 +21,8 @@ class Command(BaseCommand):
                 datafile = data_dir + line.strip().replace("\\", "/") + ".obj"
                 tag_text = line.split("\\")[0].replace("1_", "")
                 testTag = Tag(category=str(tag_text), confidence=1.0)
-                test_account = Account.objects.all()[0] 
+
+                test_account = Account.objects.all()[0]
                 convert_obj_pcd(datafile, datafile + '.pcd')
                 #import pdb; pdb.set_trace()
                 pcd_to_vfh_histogram(datafile + '.pcd', datafile + '_vfh' + '.pcd')
